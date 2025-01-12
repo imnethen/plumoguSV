59c05bbedbad98dad03ee6b5662c0722132ada7f
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-01-12 21:38:45 +0900

    Add curvature to mapping function

--- plugin.lua
+++ plugin.lua
@@ -9160,13 +9160,14 @@ function ssf(startTime, multiplier)
     return utils.CreateScrollSpeedFactor(startTime, multiplier)
 end
 
-function ssfVibrato(lowerStart, lowerEnd, higherStart, higherEnd, startTime, endTime, resolution)
+function ssfVibrato(lowerStart, lowerEnd, higherStart, higherEnd, startTime, endTime, resolution, curvature)
+    local exponent = 2 ^ (curvature / 100)
     local delta = endTime - startTime / 2 * resolution
     local time = startTime
     local ssfs = { ssf(startTime - getUsableDisplacementMultiplier(startTime), map.GetScrollSpeedFactorAt(time)) }
     while time < endTime do
-        local x = (time - startTime) - (endTime - startTime)
-        local y = (time + delta - startTime) - (endTime - startTime)
+        local x = ((time - startTime) - (endTime - startTime)) ^ exponent
+        local y = ((time + delta - startTime) - (endTime - startTime)) ^ exponent
         table.insert(ssfs, ssf(time - getUsableDisplacementMultiplier(time), higherStart + x * (higherEnd - higherStart))) -- Prevent linearization
         table.insert(ssfs, ssf(time, lowerStart + x * (lowerEnd - lowerStart)))
         table.insert(ssfs, ssf(time - getUsableDisplacementMultiplier(time), lowerStart + y * (lowerEnd - lowerStart)))    -- Prevent linearization
