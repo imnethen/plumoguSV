ad7842de7c5c6fc656784417f51621d72c729410
plummyyummy <sweetplumquaver@gmail.com>
2025-05-05 15:06:58 -0700

    Fix non-retroactive undo through tolerance

--- plugin.lua
+++ plugin.lua
@@ -5359,7 +5359,14 @@ end
 --    svsToRemove : list of SVs to remove [Table]
 --    svsToAdd    : list of SVs to add [Table]
 function removeAndAddSVs(svsToRemove, svsToAdd)
+    local tolerance = 0.04
     if #svsToAdd == 0 then return end
+    for idx, sv in pairs(svsToRemove) do
+        local baseSV = map.GetScrollVelocityAt(sv.StartTime)
+        if (math.abs(baseSV.StartTime - sv.StartTime) > tolerance) then
+            table.remove(svsToRemove, idx)
+        end
+    end
     local editorActions = {
         utils.CreateEditorAction(action_type.RemoveScrollVelocityBatch, svsToRemove),
         utils.CreateEditorAction(action_type.AddScrollVelocityBatch, svsToAdd)
