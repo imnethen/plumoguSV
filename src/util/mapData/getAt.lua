bd7e35f3495eee4f83ac6c2d06a6b385f69f25f5
plummyyummy <sweetplumquaver@gmail.com>
2025-05-25 23:04:00 -0700

    Adjust coefficients

--- src/util/mapData/getAt.lua
+++ src/util/mapData/getAt.lua
@@ -39,5 +39,5 @@ end
 function getTimingPointAt(offset)
     local line = map.GetTimingPointAt(offset)
     if line then return line end
-    return { StartTime = 0, Bpm = 100 }
+    return { StartTime = -69420, Bpm = 42.69 }
 end
