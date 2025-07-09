1423019ca0ba6dd4fd9fc3e847960fc83472f680
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-09 14:45:30 +0800

    test for duplicate code blocks

---
+++ tests/duplicated-lines.ts
@@ -0,0 +1,25 @@
+export default function checkDuplicatedLines(plugin: string[]) {
+    const hashmap = {}
+
+    plugin.forEach((line, idx) => {
+        line = line.trim()
+        const blockLength = 10
+        if (idx > plugin.length - blockLength) return
+        if (line.startsWith("--")) return
+        if (line.startsWith("return")) return
+        if (line.startsWith("imgui")) return
+        if (line.startsWith("}")) return
+        if (line.startsWith('"')) return
+        if (/^[a-zA-Z0-9_]+ = [-a-zA-Z0-9_\.\[\(\)\{\}]+,?$/.test(line)) return
+        let testStr = line
+        for (let i = 1; i < blockLength; i++) {
+            testStr = `${testStr}\n${plugin[idx + i]}`
+        }
+        if (!hashmap[testStr]) hashmap[testStr] = 0
+        hashmap[testStr]++
+    })
+
+    Object.keys(hashmap).forEach((k) => {
+        if (hashmap[k] > 1) console.log(k)
+    })
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,10 +1,10 @@
-// UNUSED FUNCTION TEST
-
 import * as fs from "fs"
 import checkUnusedFunctions from "./unused-functions"
 import checkDoubleNestedFiles from './double-nested-files'
+import checkDuplicatedLines from './duplicated-lines'
 
 const plugin = fs.readFileSync("plugin.lua", "utf-8").replaceAll("\r", "").split("\n")
 
 checkDoubleNestedFiles()
 checkUnusedFunctions(plugin)
+checkDuplicatedLines(plugin)
