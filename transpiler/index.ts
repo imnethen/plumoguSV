a1ed8a589556d5ae3f9817de3d12f39179ddc4c4
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-09 20:19:30 +0800

    add quaver intellisense module, quinsight

--- transpiler/index.ts
+++ transpiler/index.ts
@@ -24,7 +24,6 @@ export default async function transpiler(devMode = false, fuckify = false) {
     let output = "";
 
     const files = getFilesRecursively("./packages");
-    files.push(...getFilesRecursively("./intellisense"));
     files.push(...getFilesRecursively("./src"));
 
     const ignoredFiles = ["classes.lua", "intellisense.lua"];
