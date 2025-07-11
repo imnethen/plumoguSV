ffb67b19adfa24628d13d93416d57ac9770d5b9f
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-12 03:50:45 +0800

    fix builder by placing folder inside zip

--- transpiler/build.ts
+++ transpiler/build.ts
@@ -1,73 +1,79 @@
-import AdmZip = require("adm-zip");
-import transpiler from ".";
-import * as chalk from "chalk";
-import * as fs from "fs";
+import AdmZip = require('adm-zip');
+import transpiler from '.';
+import * as chalk from 'chalk';
+import * as fs from 'fs';
 
 const startTime = Date.now();
 
 console.log(
     chalk.blueBright(
-        chalk.bold(`Transpiling ${chalk.redBright("plugin.lua")}...`)
+        chalk.bold(`Transpiling ${chalk.redBright('plugin.lua')}...`)
     )
 );
 transpiler();
 
-const versionNumber = `v${fs.readFileSync(".version", "utf-8")}`;
-
+const versionNumber = `v${fs.readFileSync('.version', 'utf-8')}`;
 
 console.log(
-    chalk.blueBright(chalk.bold(`Modfifying version number of ${chalk.redBright("plugin.lua")}...`))
+    chalk.blueBright(
+        chalk.bold(
+            `Modfifying version number of ${chalk.redBright('plugin.lua')}...`
+        )
+    )
 );
 
-const pluginLines = fs.readFileSync("plugin.lua", "utf-8").split("\n");
+const pluginLines = fs.readFileSync('plugin.lua', 'utf-8').split('\n');
 
 pluginLines.forEach((line, idx) => {
-    if (line.includes("plumoguSV") && line.includes("AlwaysAutoResize")) {
-        pluginLines[idx] = pluginLines[idx].replaceAll(/"[A-z]+"/g, `"plumoguSV-${versionNumber}"`)
+    if (line.includes('plumoguSV') && line.includes('AlwaysAutoResize')) {
+        pluginLines[idx] = pluginLines[idx].replaceAll(
+            /"[A-z]+"/g,
+            `"plumoguSV-${versionNumber}"`
+        );
     }
-})
+});
 
-fs.writeFileSync("plugin.lua", pluginLines.join("\n"));
+fs.writeFileSync('plugin.lua', pluginLines.join('\n'));
 
 console.log(
-    chalk.blueBright(chalk.bold(`Updating ${chalk.redBright("README.md")}...`))
+    chalk.blueBright(chalk.bold(`Updating ${chalk.redBright('README.md')}...`))
 );
 
-const readme = fs.readFileSync("README.md", "utf-8");
+const readme = fs.readFileSync('README.md', 'utf-8');
 
 if (
-    readme.split("\n")[0].replaceAll("\r", "").split("plumoguSV ")[1] !==
+    readme.split('\n')[0].replaceAll('\r', '').split('plumoguSV ')[1] !==
     versionNumber
 ) {
-    const readmeLines = readme.split("\n");
+    const readmeLines = readme.split('\n');
     readmeLines[0] = `# plumoguSV ${versionNumber}`;
-    fs.writeFileSync("README.md", readmeLines.join("\n"));
+    fs.writeFileSync('README.md', readmeLines.join('\n'));
 }
 
 console.log(
     chalk.blueBright(
-        chalk.bold(`Updating ${chalk.redBright("package.json")}...`)
+        chalk.bold(`Updating ${chalk.redBright('package.json')}...`)
     )
 );
 
-const packageJSON = fs.readFileSync("package.json", "utf-8");
+const packageJSON = fs.readFileSync('package.json', 'utf-8');
 const idx = packageJSON
-    .replaceAll("\r", "")
-    .split("\n")
+    .replaceAll('\r', '')
+    .split('\n')
     .findIndex((l) => l.includes(`"version": `));
 const packageVer = packageJSON
-    .replaceAll("\r", "")
-    .split("\n")
+    .replaceAll('\r', '')
+    .split('\n')
     [idx].split(': "')[1]
-    .replaceAll(/\"\,/g, "");
+    .replaceAll(/\"\,/g, '');
 
 if (packageVer !== versionNumber.slice(1)) {
-    const packageLines = packageJSON.split("\n");
+    const packageLines = packageJSON.split('\n');
     packageLines[idx] = packageLines[idx].replace(
         packageVer,
         versionNumber.slice(1)
     );
-    fs.writeFileSync("package.json", packageLines.join("\n"));
+    fs.writeFileSync('package.json', packageLines.join('\n'));
 }
 
 const packageName = `plumoguSV-${versionNumber}`;
@@ -82,13 +88,14 @@ console.log(
 
 const zip = new AdmZip();
 
-if (!fs.existsSync("builds")) fs.mkdirSync("builds");
+if (!fs.existsSync('builds')) fs.mkdirSync('builds');
 if (!fs.existsSync(`build-temp`)) fs.mkdirSync(`build-temp`);
+fs.mkdirSync(`build-temp/${packageName}`);
 
-fs.copyFileSync("plugin.lua", `build-temp/plugin.lua`);
-fs.copyFileSync("settings.ini", "build-temp/settings.ini");
+fs.copyFileSync('plugin.lua', `build-temp/${packageName}/plugin.lua`);
+fs.copyFileSync('settings.ini', `build-temp/${packageName}/settings.ini`);
 
-zip.addLocalFolder("build-temp");
+zip.addLocalFolder('build-temp');
 
 zip.writeZip(`builds/${packageName}.zip`);
 
@@ -102,4 +109,4 @@ console.log(
     )
 );
 
-fs.rmSync("build-temp", { recursive: true, force: true });
+fs.rmSync('build-temp', { recursive: true, force: true });
