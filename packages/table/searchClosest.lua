a6d5540c76dd3d28838e24b4e2e680a0fda327ad
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-12 06:40:45 +0800

    i put fancy words in my commits to hide that i have no idea what i'm fucking doing

--- packages/table/searchClosest.lua
+++ packages/table/searchClosest.lua
@@ -6,7 +6,7 @@ function table.searchClosest(tbl, item)
     local leftIdx = 1
     local rightIdx = #tbl
 
-    while highIndex - lowIndex > 1 do
+    while highIdx - lowIdx > 1 do
         local middleIdx = math.floor((lowIndex + highIndex) / 2)
         if (item >= tbl[middleIndex]) then
             leftIdx = middleIdx
