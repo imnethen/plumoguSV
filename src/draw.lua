914c212161c60273045a714856e229118a5f1ce3
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-19 19:01:33 +0800

    rename themes folder to appearance, move pulse controller and background renderer

--- src/draw.lua
+++ src/draw.lua
@@ -105,38 +105,3 @@ function renderMeasureDataWidget()
     state.SetValue("oldStartOffset", startOffset)
     state.SetValue("oldEndOffset", endOffset)
 end
-
-function pulseController()
-    local prevVal = state.GetValue("prevVal", 0)
-    local colStatus = state.GetValue("colStatus", 0)
-
-    local timeOffset = 50 -- [`state.SongTime`](lua://state.SongTime) isn't entirely accurate while the song is playing, so this aims to correct that.
-
-    local timeSinceLastPulse = ((state.SongTime + timeOffset) - getTimingPointAt(state.SongTime).StartTime) %
-        ((60000 / getTimingPointAt(state.SongTime).Bpm))
-
-
-    if ((timeSinceLastPulse < prevVal)) then
-        colStatus = 1
-    else
-        colStatus = (colStatus - state.DeltaTime / (60000 / getTimingPointAt(state.SongTime).Bpm))
-    end
-
-    local futureTime = state.SongTime + state.DeltaTime * 2 + timeOffset
-
-    if ((futureTime - getTimingPointAt(futureTime).StartTime) < 0) then
-        colStatus = 0
-    end
-
-    state.SetValue("colStatus", math.max(colStatus, 0))
-    state.SetValue("prevVal", timeSinceLastPulse)
-
-    colStatus = colStatus * (globalVars.pulseCoefficient or 0)
-
-    local borderColor = state.GetValue("baseBorderColor") or vector4(1)
-    local negatedBorderColor = vector4(1) - borderColor
-
-    local pulseColor = globalVars.useCustomPulseColor and globalVars.pulseColor or negatedBorderColor
-
-    imgui.PushStyleColor(imgui_col.Border, pulseColor * colStatus + borderColor * (1 - colStatus))
-end
