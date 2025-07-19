0eb408cf8e7e6a2cb2bf802f4be26a70a49b2fb7
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-20 07:38:05 +0800

    fix duplicated line check holy fuck save me please

--- tests/duplicated-lines.ts
+++ tests/duplicated-lines.ts
@@ -5,51 +5,77 @@ export default function checkDuplicatedLines(plugin: string[]) {
     let failedTests = 0;
     let testCount = 0;
 
-    const trimmedPlugin = plugin.map((l) => l.trim());
-    trimmedPlugin.forEach((line, idx) => {
-        if (line.startsWith('--')) return;
-        if (line.startsWith('return')) return;
-        if (line.startsWith('imgui')) return;
-        if (line.startsWith('}')) return;
-        if (line.startsWith('"')) return;
-        if (/^[a-zA-Z0-9_]+ = [-a-zA-Z0-9_\.\[\(\)\{\}]+,?$/.test(line)) return;
-        if (!hashmap[line]) hashmap[line] = 0;
-        hashmap[line]++;
+    plugin.forEach((line, idx) => {
+        const trimmedLine = line.trim();
+        if (trimmedLine.startsWith('--')) return;
+        if (trimmedLine.startsWith('return')) return;
+        if (trimmedLine.startsWith('imgui')) return;
+        if (trimmedLine.startsWith('}')) return;
+        // if (trimmedLine.startsWith('"')) return;
+        // if (/^[a-zA-Z0-9_]+ = [-a-zA-Z0-9_\.\[\(\)\{\}]+,?$/.test(trimmedLine)) return;
+        if (!hashmap[trimmedLine])
+            hashmap[trimmedLine] = { indices: [], count: 0, trueLine: line };
+        hashmap[trimmedLine].count++;
+        hashmap[trimmedLine].indices.push(idx);
     });
+
+    let lines = [];
+    const indices = [];
 
     Object.keys(hashmap).forEach((k) => {
-        if (hashmap[k] < 2) {
-            delete hashmap[k];
+        if (hashmap[k].count < 2) {
             return;
         }
-        hashmap[k] = trimmedPlugin.indexOf(k);
+        for (let i = 0; i < hashmap[k].indices.length; i++)
+            lines.push(hashmap[k].trueLine);
+        indices.push(...hashmap[k].indices);
     });
 
-    const dupeArr = [];
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0; i < indices.length - 1; i++) {
+            if (indices[i] > indices[i + 1]) {
+                sorted = false;
+                const tempIdx = indices[i];
+                indices[i] = indices[i + 1];
+                indices[i + 1] = tempIdx;
+                const tempLine = lines[i];
+                lines[i] = lines[i + 1];
+                lines[i + 1] = tempLine;
+            }
+        }
+    }
+
     let pluginCursor = 0;
+    let dupeArr = [];
     let savedLines = [];
 
-    Object.entries(hashmap).forEach(([line, idx]: [string, number]) => {
+    indices.forEach((idx, trueIdx) => {
         if (idx === pluginCursor + 1) {
-            savedLines.push(line);
+            savedLines.push(lines[trueIdx]);
         } else {
-            if (savedLines.length > 4) {
+            if (savedLines.length >= 10) {
                 dupeArr.push(savedLines.join('\n'));
+                failedTests++;
             }
-            savedLines = [];
+            testCount++;
+            savedLines = [lines[trueIdx]];
         }
         pluginCursor = idx;
     });
 
+    dupeArr = [...new Set(dupeArr)];
+
     dupeArr.forEach((dupe) => {
         console.log(
-            chalk.bgRedBright(
+            chalk.bgCyanBright(
                 chalk.black(
-                    '\nThe following code block is found multiple times:\n'
+                    '\nThe following code block has been repeated in code:\n'
                 )
             )
         );
-        console.log(chalk.blueBright(dupe));
+        console.log(chalk.bgRedBright(dupe));
     });
 
     return [failedTests, testCount];
