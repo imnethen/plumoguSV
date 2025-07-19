d4f475bf7cb182dee2a33db1dec1c4567c39e08f
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-19 18:55:05 +0800

    espresso depresso

--- src/util/colorConversions.lua
+++ src/util/colorConversions.lua
@@ -1,11 +1,15 @@
 HEXADECIMAL = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" }
 
-function rgbaToUint(r, g, b, a) return a * 16 ^ 6 + b * 16 ^ 4 + g * 16 ^ 2 + r end
+function rgbaToUint(r, g, b, a)
+    local flr = math.floor
+    return flr(a) * 16 ^ 6 + flr(b) * 16 ^ 4 + flr(g) * 16 ^ 2 + flr(r)
+end
 
 function rgbaToHexa(r, g, b, a)
+    local flr = math.floor
     local hexaStr = ""
-    for _, col in pairs({ r, g, b, a }) do
-        hexaStr = hexaStr .. HEXADECIMAL[math.floor(col / 16) + 1] .. HEXADECIMAL[col % 16 + 1]
+    for _, col in ipairs({ r, g, b, a }) do
+        hexaStr = hexaStr .. HEXADECIMAL[math.floor(col / 16) + 1] .. HEXADECIMAL[flr(col) % 16 + 1]
     end
     return hexaStr
 end
@@ -17,4 +21,30 @@ function hexaToRgba(hexa)
             table.indexOf(HEXADECIMAL, hexa:charAt(i)) * 16 + table.indexOf(HEXADECIMAL, hexa:charAt(i + 1)) - 17)
     end
     return table.vectorize4(rgbaTable)
+end
+
+function rgbaToHsva(r, g, b, a)
+    local colPrime = { r / 255, g / 255, b / 255 }
+
+    local cMax = math.max(table.unpack(colPrime))
+    local cMin = math.min(table.unpack(colPrime))
+
+    local delta = cMax - cMin
+
+    local maxIdx = 1
+    local higherVal = colPrime[2]
+    local lowerVal = colPrime[3]
+    for i = 2, 3 do
+        if (colPrime[i] == cMax) then
+            maxIdx = i
+            higherVal = colPrime[i % 3 + 1]
+            lowerVal = colPrime[(i + 1) % 3 + 1]
+        end
+    end
+
+    local h = 60 * ((higherVal - lowerVal) / delta * (2 * maxIdx - 2)) % 6
+    local s = truthy(cMax) and delta / cMax or 0
+    local v = cMax
+
+    return vector.New(h, s, v, a)
 end
