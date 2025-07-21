0fca5359ab43e0f3a500a9811ea08c32c04d8a44
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-22 06:53:04 +0800

    more testwriting yayyy

--- packages/tests/expect.lua
+++ packages/tests/expect.lua
@@ -1,11 +1,14 @@
 function __toBeFunction(obtainedValue, expectedValue)
+    if (type(obtainedValue) == "number" and type(expectedValue) == "number") then
+        return math.abs(obtainedValue - expectedValue) < 1e-14
+    end
     return obtainedValue == expectedValue
 end
 
 function expect(expr)
     if (type(expr) ~= "function") then
         return {
-            toBe = function(x) return __toBeFunction(x, expr) end
+            toBe = function(x) return __toBeFunction(x, expr), { x, expr } end
         }
     else
         local fn = expr
@@ -16,7 +19,7 @@ function expect(expr)
                         toBe = function(tbl)
                             if (#tbl ~= #x) then return false end
                             for i = 1, #tbl do
-                                if (not __toBeFunction(tbl[i], fn(x[i]))) then return false end
+                                if (not __toBeFunction(tbl[i], fn(x[i]))) then return false, { i, tbl[i], fn(x[i]) } end
                             end
                             return true
                         end
--- packages/tests/index.lua
+++ packages/tests/index.lua
@@ -1,2 +1,11 @@
-require("packages.tests")
-print(expect(5).toBe(5))
+require("packages.tests.run")
+require("packages.tests.expect")
+require("packages.math.forceQuarter")
+require("packages.math.factorial")
+require("packages.math.frac")
+require("packages.table.reduce")
+
+function id(x) return x end
+
+runTest("baseline 1", expect(5).toBe(5))
+runTest("baseline 2", expect(id).of({ nil, true, 1, -5, "hi" }).toBe({ nil, true, 1, -5, "hi" }))
