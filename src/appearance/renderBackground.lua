47d2fdc900fe9dc779d274de73f5e2b2eacfa106
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-21 21:55:28 +0800

    blizzard blizzard blizzard blizzard blizzard

--- src/appearance/renderBackground.lua
+++ src/appearance/renderBackground.lua
@@ -1,7 +1,9 @@
----@class Particle
+---@class PhysicsObject
 ---@field pos Vector2
 ---@field v Vector2
 ---@field a Vector2
+
+---@class Particle: PhysicsObject
 ---@field col Vector4
 ---@field size integer
 
