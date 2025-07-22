17ba296aa0650189b724cb7f56513878a89d45e1
plummyyummy <sweetplumquaver@gmail.com>
2025-07-22 09:17:17 -0700

    update plugin again

--- packages/tests/run.lua
+++ packages/tests/run.lua
@@ -4,7 +4,7 @@ function runTest(testName, expr, res)
     if (expr) then
         print("Test " .. testName:upper() .. " was successful.")
     else
-        print("------------------------------\nTest " .. testName:upper() .. " failed.\n------------------------------")
+        print("\nTest " .. testName:upper() .. " failed.\n")
         print(table.stringify(res))
     end
 end
